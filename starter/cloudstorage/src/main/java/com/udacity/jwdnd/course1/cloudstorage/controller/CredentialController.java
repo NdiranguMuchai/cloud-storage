a3234d3478deb801de80e894dd557da6a4a7d5c4
ndirangu <ndirangu.muchai@gmail.com>
2021-02-16 12:36:41 +0300

    added user action messages to credential endpoints

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
@@ -4,17 +4,21 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Credentials;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/credentials")
 public class CredentialController {
+    private final Logger logger = LoggerFactory.getLogger(CredentialController.class);
+
     private final CredentialService credentialService;
     private final UserService userService;
 
@@ -25,26 +29,54 @@ public class CredentialController {
     @PostMapping
     public String createOrUpdateCredential(@ModelAttribute Credentials credential,
                                            Authentication authentication,
-                                           Model model){
+                                           RedirectAttributes redirectAttributes){
 
         User user = userService.getUser(authentication.getName());
         Integer userId = user.getUserId();
         credential.setUserId(userId);
 
         if (credential.getCredentialId() >0){
-            credentialService.update(credential);
+            try {
+                credentialService.update(credential);
+                redirectAttributes.addFlashAttribute("successMessage", "Credentials were successfully updated");
+                return "redirect:/result";
+
+            }catch (Exception e){
+                logger.error(e.getMessage());
+                redirectAttributes.addFlashAttribute("errorMessage", "Credentials update failed. Please try again");
+                return "redirect:/result";
+            }
+
         }else {
-            credentialService.create(credential);
+            try {
+                credentialService.create(credential);
+                redirectAttributes.addFlashAttribute("successMessage", "Credentials were successfully created");
+                return "redirect:/result";
+
+            }catch (Exception e){
+                logger.error(e.getMessage());
+                redirectAttributes.addFlashAttribute("errorMessage", "Credentials creation failed. Please try again");
+                return "redirect:/result";
+            }
         }
 
-        return "redirect:/home";
     }
 
     @RequestMapping("/{credentialId}/delete")
-    public String deleteCredential(@PathVariable Integer credentialId){
-        credentialService.deleteCredential(credentialId);
+    public String deleteCredential(@PathVariable Integer credentialId, RedirectAttributes redirectAttributes){
 
-        return "redirect:/home";
+        try {
+            credentialService.deleteCredential(credentialId);
+            redirectAttributes.addFlashAttribute("successMessage", "Credentials were successfully deleted");
+            return "redirect:/result";
+
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            redirectAttributes.addFlashAttribute("errorMessage", "Credentials delete failed. Please try again");
+            return "redirect:/result";
+        }
+
+
     }
 
 
